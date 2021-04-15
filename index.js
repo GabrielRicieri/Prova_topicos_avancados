dbf55ae8de3bd93a6c8b032d3a43d2f4b2846608
gabriel.ribeiro <gabriel.ribeiro@ifsuldeminas.edu.br>
2021-04-15 20:10:48 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bp = require('body-parser')
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 const service = require('./src/services')
 
 const router = express.Router()
--- src/services.js
+++ src/services.js
@@ -1,22 +1,76 @@
 var customers = []
-var dependents = []
 
 const getCustomersAllDependents = (req, res) => {
+    var cpf = req.params.cpf;
+    var result = [];
 
+    customers.forEach(elemento => {
+        if (cpf == elemento.cpfTitular) {
+            result.push(elemento)
+        }
+    })
+    if (result.length == 0) res.json({ message: "Customer not found" }, 404)
+    res.json(result)
 }
 
 const deleteCustomers = (req, res) => {
+    var cpf = `${req.params.cpf}`;
+    let valid = false;
+    for (let i = 0; i < customers.length; i++) {
+        if (cpf == customers[i].cpfTitular) {
+            customers.splice(i, 1);
+            valid = true;
+        }
+    }
+
+    if (!valid) res.json({ message: "Custome not found" }, 404)
+    else res.json({}, 200)
 }
 
 const getCustomersById = (req, res) => {
+    var cpf = `${req.params.cpf}`;
+    customers.forEach(elemento => {
+        console.log(cpf)
+        console.log(elemento.cpf)
+        if (cpf == elemento.cpf) res.json(elemento)
+    })
+    res.json({ message: "Customer not found" }, 404)
 }
 
 const updateCustomers = (req, res) => {
-
+    var body = req.body;
+    let valid = false;
+    for (let i = 0; i < customers.length; i++) {
+        if (cpf == customers[i].cpf) {
+            customers[i] = body;
+            valid = true;
+        }
+    }
+    if (!valid) res.json({ message: "Custome  found" }, 404)
+    res.json({}, 200)
 }
 
 const getAllCustomers = (req, res) => {
     return res.send(customers)
+}
+
+const newCustomers = (req, res) => {
+    var body = req.body;
+
+    if (body.cpfTitular) res.status(405)
+    if (body.dataNascimento) res.status(405)
+    if (body.nome) res.status(405)
+    if (body.cpf) res.status(405)
+
+    var customer = {
+        'cpfTitular': body.cpfTitular,
+        'dataNascimento': body.dataNascimento,
+        'nome': body.nome,
+        'cpf': body.cpf,
+    }
+    customers.push(customer)
+    res.json({}, 200)
+
 }
 
 
